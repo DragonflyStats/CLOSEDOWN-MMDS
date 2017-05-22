9bcd944ee8607ae70263d18d03ce28a9a242a9ca
DragonflyStats <kobriendublin@gmail.com>
2017-05-22 15:19:50 +0100

    Update Moscow1.R

--- Moscow1.R
+++ Moscow1.R
@@ -28,53 +28,30 @@ colnames(cartesian) <- c("xSlAsp","ySlAsp")
 ########################################
 
 x <- cbind(MoscowMtStJoe[,37:39],cartesian,MoscowMtStJoe[,42:64])
-head(x,3)
+
+cat("\n Inspecting the Data")
+cat("\n")
+print( head(x,3) ) 
+########################################
 
 y <- MoscowMtStJoe[,1:35]
-head(y,3)
+cat("\n")
+print( head(y,3) ) 
 
 msn <- yai(x=x, y=y, method="msn", k=1)
 # msn
 
 mal <- yai(x=x, y=y, method="mahalanobis", k=1)
 # mal
+########################################
 
 # compare these results using the generalized mean distances..
-grmsd(mal,msn)
+cat("\n")
+cat(grmsd(mal,msn))
+
+########################################
 
 # the results can be plotted.
 plot(mal,vars=yvars(mal)[1:16])
 
 #############################################
-
-# reduce the plant community data for randomForest.
-yba <- MoscowMtStJoe[,1:17]
-ybaB <- whatsMax(yba,nbig=7) # see help on whatsMax
-rf <- yai(x=x, y=ybaB, method="randomForest", k=1)
-
-# build the imputations for the original y's
-rforig <- impute(rf,ancillaryData=y)
-# compare the results using individual rmsd's
-compare.yai(mal,msn,rforig)
-
-###########################################
-
-
-
-plot(compare.yai(mal,msn,rforig))
-
-# build another randomForest case forcing regression
-# to be used for continuous variables. The answers differ
-# but one is not clearly better than the other.
-
-rf2 <- yai(x=x, y=ybaB, method="randomForest", rfMode="regression")
-rforig2 <- impute(rf2,ancillaryData=y)
-compare.yai(rforig2,rforig)
-
-
-###########################################
-
-# Method randomForest works best when there are few variables and when factors are used
-# rather than continuous variables. The whatsMax function is used to create a data frame of
-# containing a list of the species of maximum basal area, and two other related variables.
-
